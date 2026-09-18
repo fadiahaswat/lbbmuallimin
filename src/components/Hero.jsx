bcbb8c7437323b45c812384005c2f96ca91d6745
fadiahaswat <andiaqillahfadiahaswat@gmail.com>
2026-09-18 10:30:32 +0700

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,11 +32,6 @@ const CADET_FIGURES = [
     title: 'Paskibra Mu\'allimin 5',
     isLandscape: false,
   },
-  {
-    src: '/fotoslide/rusyda-pingsan.jpeg',
-    title: 'Rusyda Pingsan',
-    isLandscape: false,
-  },
 ];
 
 export default function Hero() {
